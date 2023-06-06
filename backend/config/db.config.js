7c3c4835b690bdb358d98a1f0a00d99c1cb4e514
Sachin <st19138914@gmail.com>
2023-06-06 16:17:54 +0530

    added port for deployment

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,6 +3,7 @@ export const dbConfig = {
     USER: process.env.MYSQLUSER,
     PASSWORD: process.env.MYSQLPASSWORD,
     DB: process.env.MYSQLDATABASE,
+    PORT:process.env.MYSQLPORT,
     dialect: "mysql",
     pool: {
       max: 5,
--- backend/connection.js
+++ backend/connection.js
@@ -7,6 +7,7 @@ import {Result} from './models/Result.model.js';
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  port : dbConfig.PORT,
 
   pool: {
     max: dbConfig.pool.max,
